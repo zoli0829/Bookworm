e69267809c5787a398a7cc108559d3f805699732
zoli0829 <zoli0829@gmail.com>
2025-04-23 19:35:55 +0100

    Initial commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Zoltan Vegh on 23/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
